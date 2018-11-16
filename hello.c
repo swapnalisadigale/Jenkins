cfc101467dfdba5c01f6f330bf673def481aaa15
swapnalisadigale <44576145+swapnalisadigale@users.noreply.github.com>
2018-11-16 11:46:31 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 void Hello_world(void);
-int Sum_numbers(int ,int );
+void Values(char, char);
 
 int main (void)
 {
   int sum = 0;
   Hello_world();
-  sum = Values(a,b);
+  Values(a,b);
   printf("\nSum is %d\n", sum);
   return 0;
 }
