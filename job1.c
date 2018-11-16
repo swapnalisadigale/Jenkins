5cb909c9e4a6b7a580e06967618e0b927f19d185
swapnalisadigale <44576145+swapnalisadigale@users.noreply.github.com>
2018-11-16 19:11:23 +0530

    Add files via upload

---
+++ job1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+  printf("\n\nI am JOB1\n\n");
+  return 0;
+}
---
+++ job2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+  printf("\n\nI am JOB2\n\n");
+  return 0;
+}
